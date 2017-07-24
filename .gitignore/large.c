af972e06bdf1f70663bf9e203f2bdc6bfaecaea2
DeepikaIT <30291360+DeepikaIT@users.noreply.github.com>
2017-07-24 13:24:57 +0530

    Create large.c

---
+++ .gitignore/large.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    double m1, m2, m3;
+
+    printf("Enter three different numbers: ");
+    scanf("%lf %lf %lf", &m1, &m2, &m3);
+
+    if( m1>=m2 && m1>=m3 )
+        printf("%.2f is the largest number.", m1);
+
+    if( m2>=m1 && m2>=m3 )
+        printf("%.2f is the largest number.", m2);
+
+    if( m3>=m1 && m3>=m2 )
+        printf("%.2f is the largest number.", m3);
+
+    return 0;
+}
