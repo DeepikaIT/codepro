eb320c81e226dec52cc4cd4125a959b4aa4eec0b
DeepikaIT <30291360+DeepikaIT@users.noreply.github.com>
2017-07-24 09:42:42 +0530

    Create sum.c

---
+++ .gitignore/sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,i,sum=0;
+printf("enter the positive integer");
+scanf("%d",&n);
+i=1;
+while(i<=1)
+{
+sum+=i;
+++i;
+}
+printf("%d=sum",sum);
+return 0;
+}
