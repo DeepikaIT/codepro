87e436adffafc2490f11e80dd06704d9c395fd4c
DeepikaIT <30291360+DeepikaIT@users.noreply.github.com>
2017-07-24 10:02:40 +0530

    Rename leap.c to aleap.c

---
+++ .gitignore/aleap.c
@@ -0,0 +1,19 @@
+#incude<stdio.h>
+int main()
+{
+int year;
+printf("enter a year");
+scanf("%d",&year);
+if(year4%==0)
+{
+if(year100%==0)
+{
+if(year400%==0){
+printf("%d is leap year",year);
+else
+printf("%dis not a leap year",year);
+}
+}
+}
+return 0;
+}
