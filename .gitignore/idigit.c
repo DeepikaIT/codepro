5a6deca9c9aa3ce6cb95c59bca47b7e58679d11d
DeepikaIT <30291360+DeepikaIT@users.noreply.github.com>
2017-07-24 13:30:46 +0530

    Rename digit.c to idigit.c

---
+++ .gitignore/idigit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    long long n;
+    int i = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+
+    while(n != 0)
+    {
+         n = n/10;
+        n /= 10;
+        ++i;
+    }
+
+    printf("Number of digits: %d", i);
+}
